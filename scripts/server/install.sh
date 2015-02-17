138b761ce31403c987e86e6a37a5033fd2c42269
Etiene Dalcol <dalcol@etiene.net>
2015-02-17 16:16:53 +0100

    Adding welcome page

--- scripts/server/install.sh
+++ scripts/server/install.sh
@@ -35,7 +35,7 @@ echo "
 =======================================================";
 
 #sudo apt-get update
-sudo apt-get install git git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties
+sudo apt-get install git git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties nodejs
 
 git config --global color.ui true
 git config --global user.name "Etiene Dalcol"
