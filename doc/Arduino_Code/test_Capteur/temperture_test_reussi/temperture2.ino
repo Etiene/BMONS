27be1786782e88123167726a4f13acb351775e06
nicolas2lee <nicolas2lee@gmail.com>
2015-02-17 15:13:38 +0100

    test capteur temperture reussi

--- doc/Arduino_Code/test_Capteur/temperture_test_reussi/temperture2.ino
+++ doc/Arduino_Code/test_Capteur/temperture_test_reussi/temperture2.ino
@@ -1,37 +1,38 @@
 #include <math.h>
-#define PIN_NTC A1
+#define PIN_NTC A0
+#define inPin 22
 
 double Rref=15000.0;
 double V_IN=5.0;
 
-double A_1=3.354016E-03;
-double B_1=3.349290E-04;
-double C_1=3.683843E-06;
-double D_1=7.050455E-07;
-
-double steinhartHart(double R)
+double steinhartHarteq(double R)
 {
-  double equationB1=B_1*log(R/Rref);
-  double equationC1=C_1*pow(log(R/Rref),2);
-  double equationD1=D_1*pow(log(R/Rref),3);
-  double equation=A_1+equationB1+equationC1+equationD1;
-  return pow(equation,-1);
+  double T0=273.15+25;
+  double R0=1000.0;
+  double B=3920.0;
+  double T=1.0/(1.0/T0+1.0/B*log(R/R0));
+  return T;
 } 
 
 void setup()
 {
   Serial.begin(9600);
+ // pinMode(i)
 }
 
 void loop()
 {
   double valeurAnalog=analogRead(PIN_NTC);
+  Serial.print("valeurAnalog: ");
+   Serial.println(valeurAnalog);
   double V=valeurAnalog/1024*V_IN;
+  Serial.print("V: ");
+   Serial.println(V);
   double Rth=(Rref*V)/(V_IN-V);
   Serial.print("Rth = ");
   Serial.print(Rth);
   
-  double kelvin = steinhartHart(Rth);
+  double kelvin = steinhartHarteq(Rth);
   double celsius = kelvin-273.15;
   Serial.print("Ohm - T = ");
   Serial.print(celsius);
@@ -39,3 +40,4 @@ void loop()
   
   delay(2000);
 }
+
